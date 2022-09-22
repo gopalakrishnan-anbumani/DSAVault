c50abf6e5eb86dc8255c0c0c7168888f983844ae
Gopalakrishnan Anbumani <gopalakrishnanbe16@gmail.com>
2022-09-22 17:46:30 +0530

    Optimized solution 1

--- src/com/practise/algo/searching/MedianTwoSortedArray.java
+++ src/com/practise/algo/searching/MedianTwoSortedArray.java
@@ -1,4 +1,47 @@
 class MedianTwoSortedArray {
+    
+    //Optimized Solution
+    // Own logic implemented from https://www.youtube.com/watch?v=NTop3VTjmxk
+    // Median of two Sorted Arrays of Different Sizes | Binary Search - TUF
+    public double findMedianSortedArrays1(int[] nums1, int[] nums2) {
+        int size1 = nums1.length;
+        int size2 = nums2.length;
+        if(size2 < size1){
+            return findMedianSortedArrays(nums2, nums1);
+        }
+        int i = size1 / 2;
+        int totalSize = (size1 + size2 + 1) / 2;
+        int j = totalSize - i;
+        int x = 0; int y = 0;
+        boolean exit = false;
+
+        do {
+            int l1 = (i - 1) >= 0 && size1 > 0 ? nums1[i-1] : Integer.MIN_VALUE;
+            int l2 = (j - 1) >= 0 && size2 > 0 ? nums2[j-1] : Integer.MIN_VALUE;
+            int r1 = i < size1 ? nums1[i] : Integer.MAX_VALUE;
+            int r2 = j < size2 ? nums2[j] : Integer.MAX_VALUE;
+            if(l1 > r2){
+                exit = true;
+                j+=1;
+                i-=1;
+            } else if(l2 > r1) {
+                exit = true;
+                i+=1;
+                j-=1;
+            } else {
+                exit = false;
+                x = Math.max(l1,l2);
+                y = Math.min(r1, r2);
+                System.out.println(x + " " + y);
+            }
+        } while(exit);
+        if((size1 + size2) % 2 == 0){
+            return (x+y) / 2.0;    
+        } else {
+            return x;
+        }
+    }
+    
     public double findMedianSortedArrays(int[] array1, int[] array2) {
     int l1 = array1.length;
     int l2 = array2.length;
