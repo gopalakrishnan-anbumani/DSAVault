187c0c561b5a09393796528ca92535c461108909
Gopalakrishnan Anbumani <gopalakrishnanbe16@gmail.com>
2022-09-22 23:06:11 +0530

    Added Optimized Solution

--- src/com/practise/algo/searching/FindDuplicates.java
+++ src/com/practise/algo/searching/FindDuplicates.java
@@ -1,4 +1,19 @@
 class FindDuplicates {
+    //Optimized Solution - No Extra Space. & O(n) time complexity
+    // Youtube - https://www.youtube.com/watch?v=8ci8WfQ6cns
+    public List<Integer> findDuplicates(int[] nums) {
+        List<Integer> list = new ArrayList<>();
+        for(int i=0;i<nums.length;i++){
+            int num = Math.abs(nums[i]);
+            if(nums[num-1] > 0){
+                nums[num-1] *= -1;
+            } else {
+                list.add(num);
+            }
+        }
+        return list;
+    }
+    
     // This solution is not optimized. Use Constant Space 
     public List<Integer> findDuplicates(int[] nums) {
         int count = 0;
