d4306b8f1547f898ad2f224c439e493b33883cee
Gopalakrishnan Anbumani <gopalakrishnanbe16@gmail.com>
2022-09-25 10:11:21 +0530

    Add Question Link
    Eduactive Search Topic

--- src/com/practise/algo/searching/CountElementOccurance.java
+++ src/com/practise/algo/searching/CountElementOccurance.java
@@ -34,7 +34,7 @@ class CountElementOccurance {
 
 
 Question:
-
+https://leetcode.com/problems/find-first-and-last-position-of-element-in-sorted-array/
 Given an array of integers nums sorted in non-decreasing order, find the starting and ending position of a given target value.
 
 If target is not found in the array, return [-1, -1].
