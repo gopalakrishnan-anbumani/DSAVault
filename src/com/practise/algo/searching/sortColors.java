fe7378408e4bac4063b667d25a4ce3481861f78d
Gopalakrishnan Anbumani <gopalakrishnanbe16@gmail.com>
2022-10-12 14:03:33 +0530

    Dutch National Flag problem leetcode
    https://leetcode.com/problems/sort-colors/
    Dutch National Flag problem leetcode

---
+++ src/com/practise/algo/searching/sortColors.java
@@ -0,0 +1,33 @@
+//https://leetcode.com/problems/sort-colors/
+class Solution {
+    public void sortColors(int[] nums) {
+        int low = 0;
+        int mid = 0;
+        int high = nums.length -1;
+        while(mid <= high){
+            if(nums[mid] == 0){
+                swap(nums, low, mid);
+                low++;
+                mid++;
+            } else if(nums[mid] == 1){
+                mid++;
+            } else {
+                swap(nums, mid, high);
+                high--;
+            }
+        }
+    }
+    
+    public void swap(int[] nums, int a, int b) {
+        int temp = nums[a];
+        nums[a] = nums[b];
+        nums[b] = temp;
+    }
+}
+
+
+//Given an array nums with n objects colored red, white, or blue, sort them in-place so that objects of the same color are adjacent, with the colors in the order red, white, and blue.
+
+//We will use the integers 0, 1, and 2 to represent the color red, white, and blue, respectively.
+
+//You must solve this problem without using the library's sort function.
