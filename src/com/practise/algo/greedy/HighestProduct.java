ee2ccb5ff1c30977a4ca7fe345b87903a8144c8f
Gopalakrishnan Anbumani <gopalakrishnanbe16@gmail.com>
2022-11-13 20:59:44 +0530

    Update HighestProduct.java

--- src/com/practise/algo/greedy/HighestProduct.java
+++ src/com/practise/algo/greedy/HighestProduct.java
@@ -2,6 +2,7 @@
 //https://leetcode.com/problems/maximum-product-of-three-numbers/
 
 class HighestProduct {
+    //Took 95ms
     public int maximumProduct(int[] nums) {
         Arrays.sort(nums);
         int n = nums.length;
@@ -9,6 +10,36 @@ class HighestProduct {
         int l2h1 = nums[0] * nums[1] * nums[n-1];
         return Math.max(h3, l2h1);
     }
+    //Took 6ms *Best Solution*
+    public int maximumProduct(int[] nums) {
+        int max1 = Integer.MIN_VALUE;
+        int max2 = Integer.MIN_VALUE;
+        int max3 = Integer.MIN_VALUE;
+        int min1 = Integer.MAX_VALUE;
+        int min2 = Integer.MAX_VALUE;
+        for(int n : nums){
+            if(n >= max1){
+                max3 = max2;
+                max2 = max1;
+                max1 = n;
+            } else if(n >= max2){
+                max3 = max2;
+                max2 = n;
+            } else if(n > max3) {
+                max3 = n;
+            }
+            
+            if(n <= min1){
+                min2 = min1;
+                min1 = n;
+            } else if(n < min2){
+                min2 = n;
+            }
+        }
+        return Math.max(max1*max2*max3, max1*min1*min2);
+        
+    }
+    
 }
 
 
