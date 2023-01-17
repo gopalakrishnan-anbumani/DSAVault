96ec52869e5795d638a351fe028c31e1f19ca92e
Gopalakrishnan Anbumani <gopalakrishnanbe16@gmail.com>
2023-01-17 23:12:28 +0530

    Update ValidPalindrome.java

--- src/com/practise/algo/patterns/two-pointers/ValidPalindrome.java
+++ src/com/practise/algo/patterns/two-pointers/ValidPalindrome.java
@@ -15,3 +15,34 @@ public class ValidPalindrome{
         }
         return true;
     }
+
+    
+//Palindrome Another Pblm from leetcode
+//https://leetcode.com/problems/valid-palindrome/description/
+class Solution {
+    public boolean isPalindrome(String s) {
+        String str = "";
+        for(int i=0;i<s.length();i++){
+            char c = s.charAt(i);
+            if(c >= 'a' && c <= 'z'){
+                str+=c;
+            } else if(c >= 'A' && c <= 'Z'){
+                c = (char) (c + 'a' - 'A');
+                str+=c;
+            } else if(c >= '0' && c <= '9'){
+                str+=c;
+            }
+        }
+        int start = 0;
+        int end = str.length() - 1;
+        while(start < end){
+            if(str.charAt(start) == str.charAt(end)){
+                start++;
+                end--;
+            } else {
+                return false;
+            }
+        }
+        return true;
+    }
+}
