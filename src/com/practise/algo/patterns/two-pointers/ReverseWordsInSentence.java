30e9a4816be06e36664a08c6fd1d9a2d1ff35ed3
Gopalakrishnan Anbumani <gopalakrishnanbe16@gmail.com>
2023-01-20 00:16:08 +0530

    Update ReverseWordsInSentence.java

--- src/com/practise/algo/patterns/two-pointers/ReverseWordsInSentence.java
+++ src/com/practise/algo/patterns/two-pointers/ReverseWordsInSentence.java
@@ -1,4 +1,46 @@
+//Eduactive Two Pointer Pattern
+//LeetCode - https://leetcode.com/problems/reverse-words-in-a-string/description/
 class ReverseWordsInSentence {
     public String reverseWords(String sentence) {
+                String str = "";
+        for(int i=sentence.length()-1;i>=0;i--){
+            str+=sentence.charAt(i);
+        }
+        String ans = "";
+        int start = -1;
+        int end = 0;
+        boolean count = false;
+        while(end < str.length()){
+            char c = str.charAt(end);
+            if(end == str.length()-1){
+                int i = end;
+                while(start < i){
+                        if(str.charAt(i) != ' '){
+                            ans+=str.charAt(i);
+                        }
+                        i--;
+                }
+                end++;
+            }else if (c == ' '){
+                if(!count){
+                    end++;
+                    start++;
+                } else {
+                    int i = end-1;
+                    while(start < i){
+                        ans+=str.charAt(i);
+                        i--;
+                    }
+                    ans += ' ';
+                    count = false;
+                    start=end;
+                    end++;
+                }
+            } else {
+                count = true;
+                end++;
+            }
+        }
+        return ans.charAt(ans.length()-1) == ' ' ? ans.substring(0,ans.length()-1) : ans;
     }
 }
