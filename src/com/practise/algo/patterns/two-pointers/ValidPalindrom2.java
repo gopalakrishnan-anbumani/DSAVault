66e06da5d61cb304b777104bef085c7c7d8c9a93
Gopalakrishnan Anbumani <gopalakrishnanbe16@gmail.com>
2023-01-23 23:42:22 +0530

    Update ValidPalindrom2.java

--- src/com/practise/algo/patterns/two-pointers/ValidPalindrom2.java
+++ src/com/practise/algo/patterns/two-pointers/ValidPalindrom2.java
@@ -1,10 +1,36 @@
 //https://leetcode.com/problems/valid-palindrome-ii/description/
 class ValidPalindrom2 {
-  
+  //Correct Solution
+  public boolean validPalindrome(String s) {
+        int left = 0;
+        int right = s.length() -1;
+        while(left < right){
+            if(s.charAt(left) == s.charAt(right)){
+                left++;
+                right--;
+            } else {
+                return checkPalindrome(s, left+1, right) || checkPalindrome(s, left, right-1);
+            }
+        }
+        return true;
+    }
+    public boolean checkPalindrome(String s, int i, int j){
+
+        while(i < j){
+            if(s.charAt(i) == s.charAt(j)){
+                i++;
+                j--;
+            } else {
+                return false;
+            }
+        }
+        return true;
+
+    }
   
   //Code Passed in Eduactive. Failed in LeetCode
   //Failed Input : "ebcbbececabbacecbbcbe"
-    public boolean validPalindrome(String s) {
+    public boolean validPalindrome1(String s) {
         int left = 0;
         int right = s.length() -1;
         int count = 0;
