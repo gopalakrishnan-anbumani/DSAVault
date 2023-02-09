eb648d00bc7ad716166bef35488d066321e68df9
Gopalakrishnan Anbumani <gopalakrishnanbe16@gmail.com>
2023-02-09 22:47:33 +0530

    Update RepeatedDNASequences.java

--- src/com/practise/algo/patterns/sliding-window/RepeatedDNASequences.java
+++ src/com/practise/algo/patterns/sliding-window/RepeatedDNASequences.java
@@ -3,6 +3,29 @@
 
 class RepeatedDNASequences {
   
+    //Solved using the Hash map. 
+    public List<String> findRepeatedDnaSequences(String s) {
+          List<String> result = new ArrayList<>();
+          int k = 10;
+          int n1 = s.length();
+          if(s.length() <= k){
+              return result;
+          }
+          int s1 = 0;
+          HashMap<String, Integer> hm = new HashMap();
+          while(s1+k <= n1){
+              if(hm.containsKey(s.substring(s1,s1+k)) && !result.contains(s.substring(s1,s1+k))){
+                  result.add(s.substring(s1,s1+k));
+              } else {
+                  hm.put(s.substring(s1,s1+k), 1);
+              }
+              s1++;
+          }
+
+          return result;
+      }
+    }
+  
     //My solution. LeetCode passed 30 test cases out of 31. Got Time Exceeded issue for last test case
     public List<String> findRepeatedDnaSequences1(String s) {
         List<String> result = new ArrayList<>();
