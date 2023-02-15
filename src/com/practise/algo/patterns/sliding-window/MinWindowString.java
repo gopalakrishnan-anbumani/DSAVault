99e32be75cc4f96a76ac81f66d293188b72eabe3
Gopalakrishnan Anbumani <gopalakrishnanbe16@gmail.com>
2023-02-15 22:53:46 +0530

    Update MinWindowString.java

--- src/com/practise/algo/patterns/sliding-window/MinWindowString.java
+++ src/com/practise/algo/patterns/sliding-window/MinWindowString.java
@@ -100,4 +100,64 @@ public class Solution {
     }
 }
 
+	//Sliding Window Concept - GeeksForGeeks
+class Solution1 {
+    public String minWindow(String A, String B) {
+        int m[] = new int[256];
+        char[] s = A.toCharArray();
+        char[] t = B.toCharArray();
+        // Answer
+        int ans = Integer.MAX_VALUE; // length of ans
+        int start = 0; // starting index of ans
+        int count = 0;
+        // creating map
+        for (int i = 0; i < t.length; i++) {
+            if (m[t[i]] == 0)
+                count++;
+            m[t[i]]++;
+        }
+ 
+        // References of Window
+        int i = 0;
+        int j = 0;
+ 
+        // Traversing the window
+        while (j < s.length) {
+ 
+            // Calculations
+            m[s[j]]--;
+            if (m[s[j]] == 0)
+                count--;
+ 
+            // Condition matching
+            if (count == 0) {
+                while (count == 0) {
+ 
+                    // Sorting ans
+                    if (ans > j - i + 1) {
+                        ans = Math.min(ans, j - i + 1);
+                        start = i;
+                    }
+ 
+                    // Sliding I
+                    // Calculation for removing I
+                    m[s[i]]++;
+                    if (m[s[i]] > 0)
+                        count++;
+ 
+                    i++;
+                }
+            }
+            j++;
+        }
+ 
+        if (ans != Integer.MAX_VALUE)
+            return String.valueOf(s).substring(start,
+                                               ans + start);
+        else
+            return ""; 
+    }
+}
+	
+
 }
