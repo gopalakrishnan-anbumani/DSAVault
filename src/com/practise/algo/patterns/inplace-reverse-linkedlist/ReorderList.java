33e47667aa1f632aa091195a3b7d7ae83022863c
Gopalakrishnan Anbumani <gopalakrishnanbe16@gmail.com>
2023-02-26 15:32:33 +0530

    Update ReorderList.java

--- src/com/practise/algo/patterns/inplace-reverse-linkedlist/ReorderList.java
+++ src/com/practise/algo/patterns/inplace-reverse-linkedlist/ReorderList.java
@@ -2,7 +2,39 @@
 //Eduactive - Patterns - Inplace Reversal Linkedlist
 
 class ReorderList {
+    
+    //Reverse the Second Part. & Merge it
     public void reorderList(ListNode head) {
+        ListNode slow=head;
+        ListNode fast=head;
+        while(fast.next != null && fast.next.next != null){
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+        ListNode curr = slow.next;
+        ListNode prev = null;
+        while(curr != null){
+            ListNode next = curr.next;
+            curr.next = prev;
+            prev = curr;
+            curr = next;
+        }
+        slow.next=null;
+        ListNode reversedList = prev;
+        ListNode ptr = head;
+        while(ptr != null && reversedList != null){
+            ListNode next = ptr.next;
+            ListNode nodeToInsert = reversedList;
+            reversedList = reversedList.next;
+            ptr.next = nodeToInsert;
+            nodeToInsert.next = next;
+            ptr = next;
+        }
+    }
+    
+    
+    //Solution using the Stack
+    public void reorderList1(ListNode head) {
         ListNode slow=head;
         ListNode fast=head;
         while(fast.next != null && fast.next.next != null){
