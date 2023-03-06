d150d18b9f25519e91adafd282314781aa3f4c8e
Gopalakrishnan Anbumani <gopalakrishnanbe16@gmail.com>
2023-03-06 19:51:07 +0530

    1539. Kth Missing Positive Number
    https://leetcode.com/problems/kth-missing-positive-number/description/

---
+++ leetcode-practise/Kth Missing Positive Number.java
@@ -0,0 +1,23 @@
+//1539. Kth Missing Positive Number
+//https://leetcode.com/problems/kth-missing-positive-number/description/
+
+class Solution {
+    public int findKthPositive(int[] arr, int k) {
+        int currentNumber = 1;
+        int i = 0;
+        int missed = 0;
+        while(i <= 1000){
+            if(i < arr.length && arr[i] == currentNumber){
+                currentNumber += 1;
+                i++;
+            } else {
+                currentNumber += 1;
+                missed++;
+            }
+            if(missed == k){
+                return currentNumber-1;
+            }
+        }
+        return 0;
+    }
+}
