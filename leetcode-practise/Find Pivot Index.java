740bca82ea0b79559d2bd653ec8005358a0dd171
Gopalakrishnan Anbumani <gopalakrishnanbe16@gmail.com>
2023-03-10 22:16:30 +0530

    724. Find Pivot Index

---
+++ leetcode-practise/Find Pivot Index.java
@@ -0,0 +1,19 @@
+//724. Find Pivot Index
+//https://leetcode.com/problems/find-pivot-index/
+//LeetCode 75 Course
+
+class Solution {
+    public int pivotIndex(int[] nums) {
+        int leftSum = 0;
+        int rightSum =  0;
+        for(int i=0;i<nums.length;i++){
+            rightSum+=nums[i]; 
+        }
+        for(int i = 0;i<nums.length;i++){
+            rightSum-=nums[i];
+            if(rightSum == leftSum) return i;
+            leftSum+=nums[i];
+        }
+        return -1;
+    }
+}
