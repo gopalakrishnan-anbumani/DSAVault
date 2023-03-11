0f3ddb1c45f758bc85fcc82b2769048380757844
Gopalakrishnan Anbumani <gopalakrishnanbe16@gmail.com>
2023-03-11 21:53:08 +0530

    Update Merge Sorted Array.java

--- leetcode-practise/Merge Sorted Array.java
+++ leetcode-practise/Merge Sorted Array.java
@@ -1,7 +1,25 @@
 //88. Merge Sorted Array
 //https://leetcode.com/problems/merge-sorted-array/description/
-//Leetcode 75
+//Eduactive - Patterns - K way Merge
 
+//Solution in Eduactive
+class Solution {
+    public void merge(int[] nums1, int m, int[] nums2, int n) {
+        int n1 = m-1;
+        int n2 = n-1;
+        int p = m+n-1;
+        while(n2 >= 0){
+            if(n1 >= 0 && nums1[n1] > nums2[n2]){ 
+                nums1[p] = nums1[n1];
+                n1--;
+            } else {
+                nums1[p] = nums2[n2];
+                n2--;
+            } 
+            p--;
+        }
+    }
+}
 
 //My Solution
 class Solution {
