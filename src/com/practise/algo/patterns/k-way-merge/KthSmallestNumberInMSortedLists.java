10f6dfbabb51eee81d8ba6b99b248f1a4489c0f4
Gopalakrishnan Anbumani <gopalakrishnanbe16@gmail.com>
2023-03-13 23:10:26 +0530

    Update KthSmallestNumberInMSortedLists.java

--- src/com/practise/algo/patterns/k-way-merge/KthSmallestNumberInMSortedLists.java
+++ src/com/practise/algo/patterns/k-way-merge/KthSmallestNumberInMSortedLists.java
@@ -4,7 +4,9 @@
 //Eduactive - Patterns - K way Merge
 
 class FindSmallestNumber {
-  public static int kSmallestNumber(List<List<Integer>> lists, int k) {
+  
+  //This solution put all the values in priority Queue. From Queue, get the kth element. Added Optimized Solution below
+  public static int kSmallestNumber1(List<List<Integer>> lists, int k) {
         PriorityQueue<Integer> pq = new PriorityQueue<>();
         for(int i=0;i<lists.size();i++){
             int col = lists.get(i).size();
@@ -20,4 +22,26 @@ class FindSmallestNumber {
         }
         return res;
   }
+  
+  
+  //Using K way merge. Insert only 1st element from all the array.
+  public static int kSmallestNumber(List<List<Integer>> lists, int k) {
+            PriorityQueue<int[]> pq = new PriorityQueue<>((a,b) -> a[0] - b[0]);
+	        for(int i=0;i<lists.size();i++){
+	            pq.add(new int[] {lists.get(i).get(0), i, 0});
+	        }
+	        int count = 0;
+	        int[] res = null;
+	        while(!pq.isEmpty() && count < k){
+	            res = pq.poll();
+	            count++;
+	            int row = res[1];
+	            int index = res[2];
+	            if(lists.get(row).size() > index+1) {
+	            	pq.add(new int[] {lists.get(row).get(index+1), row, index+1});
+	            }
+	        }
+	        return res == null ? 0 : res[0];
+  }
+
 }
