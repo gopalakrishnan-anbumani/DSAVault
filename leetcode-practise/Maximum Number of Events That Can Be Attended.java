2e2172c783b602af1face29c05361596958b55f4
Gopalakrishnan Anbumani <gopalakrishnanbe16@gmail.com>
2023-04-22 23:03:26 +0530

    Update Maximum Number of Events That Can Be Attended.java

--- leetcode-practise/Maximum Number of Events That Can Be Attended.java
+++ leetcode-practise/Maximum Number of Events That Can Be Attended.java
@@ -6,27 +6,28 @@ class Solution {
     public int maxEvents(int[][] events) {
         Arrays.sort(events, new Comparator<int[]>(){
             public int compare(int[] a, int[] b){
-                if(a[1] != b[1]){
+                if(a[0] == b[0]){
                     return a[1] - b[1];
-                } else {
-                    return a[0] - b[0];
                 }
+                return a[0] - b[0];
             }
         });
-        for(int[] event : events){
-        System.out.println(Arrays.toString(event));
-        }
+        //for(int[] event : events){
+            //System.out.println(Arrays.toString(event));
+        //}
         int result = 0;
-        int lastDay = -1;
-        for(int[] event : events){
-            int eventStart = event[0];
-            int eventEnd = event[1];
-            for(int i = eventStart; i <= eventEnd ; i++){
-                if(i > lastDay){
-                    result += 1;
-                    lastDay = i;
-                    break;
-                }
+        int j = 0;
+        PriorityQueue<Integer> pq = new PriorityQueue<>();
+        for(int i=1;i<100001;i++){
+            while(!pq.isEmpty() && pq.peek() < i){
+                pq.poll();
+            }
+            for(; j < events.length && events[j][0]==i; j++){
+                pq.offer(events[j][1]);
+            }
+            if(!pq.isEmpty()){
+                pq.poll();
+                result++;
             }
         }
         return result;
