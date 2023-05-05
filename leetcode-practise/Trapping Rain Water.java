aee004d01c1fab4fcf4a738bb7976ca55e9eb8be
Gopalakrishnan Anbumani <gopalakrishnanbe16@gmail.com>
2023-05-05 22:59:19 +0530

    Update Trapping Rain Water.java

--- leetcode-practise/Trapping Rain Water.java
+++ leetcode-practise/Trapping Rain Water.java
@@ -1,36 +1,26 @@
 //42. Trapping Rain Water
 //https://leetcode.com/problems/trapping-rain-water/
 
-
 class Solution {
     public int trap(int[] height) {
         int n = height.length;
         int res = 0;
-        int l=0;
-        int r=0;
-        while(l < n){
-            while(height[l] == 0){
-                l++;
-            }
-            r=l+1;
-            while(r < n && height[r] < height[l]){
-                r++;
-            }
-            
-            if(r >= n){
-                return res;
-            }
-            System.out.println("l ="+height[l] + " r= "+height[r]);
-            int min = Math.max(height[r],height[l]);
-            l++;
-            r--;
-            while(l <= r){
-                res += (min - height[l]);
-                l++;
-            }
-            r=r+2;
-            l=r;
-            System.out.println("l. ="+l + " r.= "+r);
+        
+        int[] lHeights = new int[n];
+        lHeights[0] = height[0];
+        for(int i=1; i<n;i++){
+            lHeights[i] = Math.max(lHeights[i-1], height[i]);
+        }
+
+        int[] rHeights = new int[n];
+        rHeights[n-1] = height[n-1];
+        for(int i=n-2; i>=0; i--){
+            rHeights[i] = Math.max(rHeights[i+1], height[i]);
+        }
+
+        for(int i=1;i<n-1;i++){
+            int min = Math.min(lHeights[i],rHeights[i]);
+            res = res + min - height[i];
         }
         return res;
     }
