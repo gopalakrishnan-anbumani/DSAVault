afdfe619f11918875efa62ef0493966796d8abac
Gopalakrishnan Anbumani <gopalakrishnanbe16@gmail.com>
2023-05-27 22:25:58 +0530

    Update Remove Element.java

--- leetcode-practise/Remove Element.java
+++ leetcode-practise/Remove Element.java
@@ -2,6 +2,16 @@
 //https://leetcode.com/problems/remove-element/description/?envType=study-plan-v2&id=top-interview-150
 
 class Solution {
+    
+     public int removeElement1(int[] nums, int val) {
+        int k = 0;
+        for(int i=0;i<nums.length;i++){
+            if(nums[i]!=val){
+                nums[k++] = nums[i];
+            }
+        }
+        return k;
+    }
     public int removeElement(int[] nums, int val) {
         int k = 0;
         int start = 0;
