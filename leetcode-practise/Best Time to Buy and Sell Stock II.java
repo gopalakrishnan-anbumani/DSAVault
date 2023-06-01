84a3f1ae08b529cf769a5d64a43279a19aa7d179
Gopalakrishnan Anbumani <gopalakrishnanbe16@gmail.com>
2023-06-01 20:30:57 +0530

    Update Best Time to Buy and Sell Stock II.java

--- leetcode-practise/Best Time to Buy and Sell Stock II.java
+++ leetcode-practise/Best Time to Buy and Sell Stock II.java
@@ -2,7 +2,20 @@
 //https://leetcode.com/problems/best-time-to-buy-and-sell-stock-ii/description/
 
 class Solution {
+    
+    //Simple Logic
     public int maxProfit(int[] prices) {
+        int profit = 0;
+        for(int i=1;i<prices.length;i++){
+            if(prices[i] - prices[i-1] > 0){
+                profit += prices[i] - prices[i-1];
+            }
+        }
+        return profit;
+    }
+
+    //Added more conditios to check
+    public int maxProfit1(int[] prices) {
         int overAllProfit = 0;
         int stock = prices[0];
         int pProfit = 0;
