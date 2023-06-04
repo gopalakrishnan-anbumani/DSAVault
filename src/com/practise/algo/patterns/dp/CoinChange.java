b264324920ee0d38956d08632f8b16e6c2e40fe2
Gopalakrishnan Anbumani <gopalakrishnanbe16@gmail.com>
2023-06-04 22:53:46 +0530

    322. Coin Change

---
+++ src/com/practise/algo/patterns/dp/CoinChange.java
@@ -0,0 +1,21 @@
+//322. Coin Change
+//https://leetcode.com/problems/coin-change/description/
+
+
+class Solution {
+    public int coinChange(int[] coins, int amount) {
+        int[] dp = new int[amount+1];
+        Arrays.fill(dp,amount+1);
+        dp[0]=0;
+        for(int i=1;i<amount+1;i++){
+            for(int coin : coins){
+                int diff = i - coin;
+                if(diff >= 0){
+                    dp[i] = Math.min(dp[i], 1+dp[diff]);
+                }
+            }
+        }
+        //System.out.println(Arrays.toString(dp));
+        return dp[amount] != amount+1 ? dp[amount] : -1;
+    }
+}
