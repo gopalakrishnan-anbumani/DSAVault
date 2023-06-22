62e7e092f7d7e4f7a69324385cb243536cb6d5a0
Gopalakrishnan Anbumani <gopalakrishnanbe16@gmail.com>
2023-06-22 23:07:39 +0530

    Update UniquePathsII.java

--- leetcode-practise/UniquePathsII.java
+++ leetcode-practise/UniquePathsII.java
@@ -2,6 +2,28 @@
 //63. Unique Paths II
 
 class Solution {
+
+    //NeetCode
+    public int uniquePathsWithObstacles(int[][] grid) {
+        int m = grid.length;
+        int n = grid[0].length;
+        
+        int[] dp = new int[n];
+        dp[n-1]=1;
+        for(int r=m-1;r>=0;r--){
+            for(int c=n-1;c>=0;c--){
+                if(grid[r][c]==1){
+                    dp[c]=0;
+                } else if(c+1 < n){
+                    dp[c] = dp[c] + dp[c+1];
+                }
+            }
+        }
+        return dp[0];
+    }
+
+
+    
     public int uniquePathsWithObstacles(int[][] obstacleGrid) {
         int m = obstacleGrid.length;
         int n = obstacleGrid[0].length;
