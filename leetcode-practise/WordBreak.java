bbebc3d2ba808192037c517d3aed9badbdf77f1a
Gopalakrishnan Anbumani <gopalakrishnanbe16@gmail.com>
2023-07-08 21:31:12 +0530

    139. Word Break

--- leetcode-practise/WordBreak.java
+++ leetcode-practise/WordBreak.java
@@ -1,6 +1,26 @@
 //139. Word Break
 //https://leetcode.com/problems/word-break/description/
 
+//NeetCode Solution
+class Solution {
+    public boolean wordBreak(String s, List<String> wordDict) {
+        boolean[] dp = new boolean[s.length()+1];
+        dp[s.length()] = true;
+
+        for(int i=s.length()-1; i>=0; i--){
+            for(String str : wordDict){
+                if(i+str.length() <= s.length() && s.substring(i, i+str.length()).equals(str)){
+                    dp[i] = dp[i+str.length()];
+                    if(dp[i]){
+                        break;
+                    }
+                }
+            }
+        }
+
+        return dp[0];
+    }
+}
 
 class Solution {
     List<String> list = null;
