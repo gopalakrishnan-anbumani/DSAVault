72ad46a25e48c9fb240abfc9100ab65f8893aea2
Gopalakrishnan Anbumani <gopalakrishnanbe16@gmail.com>
2023-08-19 23:30:15 +0530

    50. Pow(x,n)

---
+++ leetcode-practise/Pow.java
@@ -0,0 +1,16 @@
+//50. Pow(x, n)
+//https://leetcode.com/problems/powx-n/description/
+
+
+class Pow {
+  public double myPow(double x, long n) {
+    if (n == 0)
+      return 1;
+    if (n < 0)
+      return 1 / myPow(x, -n);
+    if (n % 2 == 1)
+      return x * myPow(x, n - 1);
+    return myPow(x * x, n / 2);
+  }
+}
+
